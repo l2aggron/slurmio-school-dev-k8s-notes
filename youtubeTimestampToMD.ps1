63ae8d6769514ef349a819e3615294ae95a956cc
l2aggron <l2aggron@gmail.com>
2021-11-02 01:38:50 +0300

    Create youtubeTimestampToMD.ps1
    Скрипт для упрощения добавления таймкодов в конспект

---
+++ youtubeTimestampToMD.ps1
@@ -0,0 +1,10 @@
+﻿# Конвертируем ссылку с привязкой ко времени в markdown таймкод
+while ($true) {
+    $clipboard = Get-Clipboard
+    if ($clipboard -match "^https:\/\/youtu\.be\/.+\?t=\d+$") {
+        $totalSeconds = ($clipboard -split "t=")[1]
+        $timestamp = (New-TimeSpan -Start (Get-Date 0) -End (Get-Date 0).AddSeconds($totalSeconds)).ToString()
+        "[$timestamp]($clipboard)" | Set-Clipboard
+    }
+    sleep -Milliseconds 200
+}
